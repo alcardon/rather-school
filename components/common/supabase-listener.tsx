79ccdc5f50c463f7cebc4013b3e69c20eadba512
AlejandroCRios <al.cardonarios@gmail.com>
2023-04-04 19:59:31 -0500

    Move supabase listener/provider to common comp

--- components/common/supabase-listener.tsx
+++ components/common/supabase-listener.tsx
@@ -3,6 +3,7 @@
 import { useRouter, usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { useSupabase } from "./supabase-provider";
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 export default function SupabaseListener({
   serverAccessToken,
@@ -13,15 +14,23 @@ export default function SupabaseListener({
   const router = useRouter();
   const pathname = usePathname();
 
+
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((event, session) => {
       if (session?.access_token !== serverAccessToken) {
         router.refresh();
+
       }
-      if (!session?.access_token) {
+      if (!session?.user) {
         router.push("/login");
+      }
+      if (event == "SIGNED_OUT") {
+        router.push("/login");
+      }
+      if (event == "SIGNED_IN") {
+        router.push("/");
       }
       if (session && !pathname.startsWith("/dashboard")) {
         router.push("/dashboard");
